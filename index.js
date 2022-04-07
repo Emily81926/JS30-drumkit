dd678429ef4aeea86cf704980061c23752ef71d1
Emily81926 <emily81926@gmail.com>
2022-04-07 22:04:24 +0800

    feat: add press the key to make drum sound feature

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const keyboard = document.querySelectorAll('.key')
-
-for (let i = 0; i < keyboard.length; i++) {
-  const sound = keyboard[i]
-
-  //在所有按鍵加上監聽
-  sound.addEventListener('click', (e) => {
-    //取得按鍵的data-key，並存成audioNumber
-    const audioNumber = sound.dataset.key
-    
-    //播放與按鍵相同data-key的鼓聲
-    makeTheBeat(audioNumber)
-  })
-}
-
 //播放鼓聲
 function makeTheBeat(keycode) {
-   //宣告音檔的dataset.key跟keyboard的dataset.key相同，則播放該鼓聲
+  //宣告音檔的dataset.key跟keyboard的dataset.key相同，則播放該鼓聲
   const audio = document.querySelector(`audio[data-key="${keycode}"]`)
   audio.play()
 }
+
+
+document.addEventListener('keydown', (e) => {
+  //當按按鍵的時候可以取得該keycode
+  const code = e.keyCode //為什麼keycode會被劃掉？
+  //當按鍵的keycode跟音擋的keycode相同時，播放音樂
+  makeTheBeat(code)
+})
 
