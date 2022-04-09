11777df664cbc45edabae13551adbe30eac117c0
Emily81926 <emily81926@gmail.com>
2022-04-09 12:16:18 +0800

    feat: add currentTime equals to 0 to make the drum sound restart

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 function makeTheBeat(keycode) {
   //宣告音檔的dataset.key跟keyboard的dataset.key相同，則播放該鼓聲
   const audio = document.querySelector(`audio[data-key="${keycode}"]`)
+  audio.currentTime = 0
   audio.play()
 }
-
 
 document.addEventListener('keydown', (e) => {
   //當按按鍵的時候可以取得該keycode
@@ -15,7 +15,4 @@ document.addEventListener('keydown', (e) => {
   //當按鍵的keycode跟音擋的keycode相同時，播放音樂
   makeTheBeat(code)
 })
-
-
-//當播放鼓聲後，鼓聲會停止播放
 
